f72c1d1d35964e09ebfd563092a291f928af59d3
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-15 22:08:32 +0700

    create user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
+import { UserService } from "./modules/user/user.service";
 
 @Module({
   imports: [
@@ -10,6 +11,6 @@ import { ConfigModule } from "@nestjs/config";
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
