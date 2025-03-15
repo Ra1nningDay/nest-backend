9527065c463f1539f923e01b2b0815b6e6f2a925
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-15 21:27:38 +0700

    install prisma for data management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,14 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
-import { AuthModule } from "./modules/auth/auth/auth.module";
-import { AuthController } from "./mudules/auth/auth/auth.controller";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    AuthModule,
   ],
-  controllers: [AppController, AuthController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
