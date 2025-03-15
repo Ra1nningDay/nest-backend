7500e3db46362a15d7364043c5526a15ab9d26a6
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-15 18:43:55 +0700

    built module auth

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,4 +7,4 @@ async function bootstrap() {
   await app.listen(3000);
 }
 
-bootstrap();
+bootstrap().catch((err) => console.error(err));
