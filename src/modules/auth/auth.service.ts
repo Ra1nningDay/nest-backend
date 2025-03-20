a97f2444d12be5d023584300ee79deaa59c33110
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-20 23:48:25 +0700

    feat: use jwt for auth

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,23 +2,27 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UserService } from "../user/user.service";
 import * as brcypt from "bcrypt";
 import { SignInDto } from "./dto/signIn.dto";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async signIn(signInDto: SignInDto): Promise<any> {
+  async signIn(signInDto: SignInDto): Promise<{ access_token: string }> {
     const user = await this.userService.userByEmail(signInDto.email);
     if (user?.password !== signInDto.pass) {
       throw new UnauthorizedException();
     }
 
-    const { password, ...result } = user;
-
-    const checkedPass = await brcypt.compare(password, user?.password);
+    const checkedPass = await brcypt.compare(signInDto.pass, user.password);
 
     if (!checkedPass) throw new UnauthorizedException();
 
-    return result;
+    const payload = { sub: user.id, username: user.email };
+
+    return { access_token: await this.jwtService.signAsync(payload) };
   }
 }
